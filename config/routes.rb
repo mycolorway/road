50fc6e3834be81f5fdfc9f2dc336ea2265c28d1a
Ruochen <ruochen@mycolorway.com>
2013-07-20 18:20:31 +0800

    by_bounds页面，无地图

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,16 @@ Road::Application.routes.draw do
     resources :path_nodes, :pois, :photos
   end
 
+  namespace :m do
+    resources :stories, shallow: true, constraints: { id: /\d+/ } do
+      collection do
+        get :by_bounds, :by_keyword
+      end
+
+      resources :path_nodes, :pois, :photos
+    end
+  end
+
   # ----
   # misc
   # ----
