302369129ee428c8e39d278ea06257df484e63e0
Snow Hellsing <snow.hellsing@gmail.com>
2013-07-20 13:05:15 +0800

    NEW: init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Road::Application.config.secret_token = '1c15a5d3ab727bc2f297df85151972dcda8425c5f7d4bef86a87b734d85f3c7cbb47a002e51b23ca767e857bc64fd1a8f0bc22f7412623cdbd40bb037c87cd08'
